c01bcb9bf4c1efb1338518c66caf59f2a3e121ab
Vaturi <omri1991@gmail.com>
2022-12-19 14:38:49 +0200

    update some code

--- Wehicle.java
+++ Wehicle.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public abstract class Wehicle extends Thread{
+public abstract class Wehicle extends Thread {
     String type;
     double avgTimeToBeWash;
     Random rnd;
@@ -10,15 +10,42 @@ public abstract class Wehicle extends Thread{
     }
 
     public abstract void wehicleArraiv();
+
     public abstract void wehicleEnter_washer();
+
     public abstract void wehicle_finish();
+
+    public Wehicle chosenWehicle() {
+        Random rnd = new Random();
+        int n = rnd.nextInt(3) + 1;
+        Wehicle w = null;
+        switch (n) {
+            case 0:
+                w = new Truck();
+                break;
+            case 1:
+                w = new SUV();
+                break;
+            case 2:
+                w = new Car();
+                break;
+
+            case 3:
+                w = new MiniBus();
+                break;
+
+        }
+        return w;
+
+    }
 
     @Override
     public void run() {
         try {
-            sleep((long) (Math.log(-rnd.nextDouble())/avgTimeToBeWash));
+            sleep((long) (Math.log(-rnd.nextDouble()) / avgTimeToBeWash));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
     }
 }
--- WehicleWasher.java
+++ WehicleWasher.java
@@ -2,66 +2,69 @@ import java.io.File;
 import java.util.*;
 
 public class WehicleWasher {
-    ArrayList<Wehicle> whashPlace;
-    Queue<Wehicle> line;
-    ArrayList<Wehicle> inWash;
-    Queue<Car> cars;
-    Queue<SUV> SUVs;
-    Queue<MiniBus> miniBuses;
-    Queue<Truck> trucks;
+    ArrayList<Wehicle> wash_station;
+    ArrayList<Wehicle> line_to_wash;
+    ArrayList<Wehicle> wehicles_in_wash;
+    ArrayList<Car> cars_after_wash;
+    ArrayList<SUV> SUVs_after_wash;
+    ArrayList<MiniBus> miniBuses_after_wash;
+    ArrayList<Truck> trucks_after_wash;
+
+    WehicleLogger log_w_r;
+
     int needToBeWash;
     double avgBetweenCars;
     double avgTimeToWash;
     Random rnd;
 
-    public WehicleWasher(int places, int needToBeWash, double avgBetweenCars, double timeForWash) {
-        this.avgBetweenCars = timeForWash;
-        this.needToBeWash = needToBeWash;
-        this.avgBetweenCars = avgBetweenCars;
-        whashPlace = new ArrayList<Wehicle>();
-        line = new LinkedList<Wehicle>();
-        inWash = new ArrayList<Wehicle>();
-        cars = new LinkedList<Car>();
-        SUVs = new LinkedList<SUV>();
-        miniBuses = new LinkedList<MiniBus>();
-        trucks = new LinkedList<Truck>();
-
+    public WehicleWasher(int num_of_station, int num_wehicle_to_wash) {
+        wash_station = new ArrayList<>(num_of_station);
+        this.needToBeWash = num_wehicle_to_wash;
+        line_to_wash = new ArrayList<>();
+        wehicles_in_wash = new ArrayList<>();
 
     }
+
 
     public void proses() throws InterruptedException {
         int lineCounter = 0;
         while (lineCounter < needToBeWash) {
             Thread.sleep((long) (-Math.log(rnd.nextDouble()) / avgBetweenCars));
-            line.add(chosenWehicle());
+            line_to_wash.add(chosenWehicle());
             lineCounter++;
 
 
         }
     }
 
-    public boolean empty_spot(){
-        if(whashPlace.isEmpty() && whashPlace.get(needToBeWash-1) !=null){
+    public boolean empty_spot() {
+        if (wash_station.isEmpty() && wash_station.get(needToBeWash - 1) != null) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
 
     public synchronized void q_to_washer(Wehicle w) throws InterruptedException {
-        if(empty_spot() == false){
-            line.add(w);
+        if (empty_spot() == false) {
+            line_to_wash.add(w);
             w.wait();
-        }else {
-            inWash.add(w);
+        } else {
+            wehicles_in_wash.add(w);
         }
 
     }
 
+    public void new_wehicle_toWash() {
+        if (empty_spot()) {
+            wash_station.add(line_to_wash.get(0));
+        }
+    }
+
     public Wehicle chosenWehicle() {
         Random rnd = new Random();
-        int n = rnd.nextInt(3);
-        Wehicle w;
+        int n = rnd.nextInt(3) + 1;
+        Wehicle w = null;
         switch (n) {
             case 0:
                 w = new Truck();
@@ -73,7 +76,7 @@ public class WehicleWasher {
                 w = new Car();
                 break;
 
-            default:
+            case 3:
                 w = new MiniBus();
                 break;
 
