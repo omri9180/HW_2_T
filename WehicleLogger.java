e3aacb2093508404a88352ed76c8d3ed1bc3be3a
Vaturi <omri1991@gmail.com>
2022-12-19 13:12:12 +0200

    update logger

--- WehicleLogger.java
+++ WehicleLogger.java
@@ -1,26 +1,32 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class WehicleLogger {
+    File fileName = new File("log.txt");
+    private FileWriter log_Writer;
 
-    File file = new File("log.txt");
+    private FileReader log_Reader = new FileReader(fileName);
+    private BufferedReader read;
 
-    Wehicle wehicle;
-
-
-    public void WriteToFileLog(){
-        String log = this.wehicle.type +" ID: " + wehicle.threadId() + "\n";
-        try {
-            PrintWriter wr = new PrintWriter(file);
-            wr.write(log);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-
+    public WehicleLogger() throws FileNotFoundException {
     }
 
+    public void log_Writer(String log) throws IOException {
+        log_Writer = new FileWriter(fileName);
+        log_Writer.write(log + "\n");
+
+        log_Writer.close();
+    }
+
+
+    public void read() throws IOException {
+        log_Reader = new FileReader(fileName);
+        read = new BufferedReader(log_Reader);
+        if (read.ready()) {
+            while (read.ready()) {
+                System.out.println(read.readLine());
+            }
+        }
+        read.close();
+        log_Reader.close();
+    }
 }
